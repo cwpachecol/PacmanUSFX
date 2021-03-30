bbb7ccbc04f676a5c8247ab46b9ab695d3c97310
cwpachecol <pacheco.carlos@usfx.bo>
2021-03-30 16:11:09 -0400

    primera clase juego

---
+++ PPacmanUSFX/Pacman.h
@@ -0,0 +1,14 @@
+#pragma once
+class Pacman
+{
+private:
+	float posicionX;
+	float posicionY;
+	int color;
+	float velocidadX;
+	float velocidadY;
+public:
+	void move(float _velocidadX, float _velocidadY);
+
+};
+
