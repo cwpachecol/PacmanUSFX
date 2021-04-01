08252ce30d66c214606e5f698fb78f63193134d7
cwpachecol <pacheco.carlos@usfx.bo>
2021-04-01 16:01:43 -0400

    Implementación de SDL_Point en clase Fruta

--- PPacmanUSFX/Fruta.h
+++ PPacmanUSFX/Fruta.h
@@ -1,21 +1,18 @@
 #pragma once
+#include <SDL.h>
 
 
 class Fruta
 {
 private:
 	int color;
-	int posicionX;
-	int posicionY;
+	SDL_Point posicion;
 public:
 	int getColor() { return color; }
 	void setColor(int _color) { color = _color; }
 
-	int getPosicionX() { return posicionX; }
-	void setPosicionX(int _posicionX) { posicionX = _posicionX; }
-
-	int getPosicionY() { return posicionY; }
-	void setPosicionY(int _posicionY) { posicionY = _posicionY; }
+	SDL_Point getPosicion() { return posicion; }
+	void setPosicion(SDL_Point _posicion) { posicion = _posicion; }
 
 	//Metodos accesores de la propiedad tipo
 	int getTipo() { return tipo; }
